2eddc5674f9c054752044033f8c5736df0cdf336
zanepetersen <zpetersen.22@gmail.com>
2015-04-03 18:07:21 -0500

    Initial Commit

---
+++ ENMN/Controllers/MessageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ENMN.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ENMN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ENMN.MvcApplication" Language="C#" %>
